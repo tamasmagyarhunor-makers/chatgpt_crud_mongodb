d40953b177c271d03990219f29184b9b25b63ef4
Magyar-Hunor Tamas <magyar-hunor.tamas@makers.tech>
2023-01-21 11:23:32 +0000

    Update Car model;

--- models/car.py
+++ models/car.py
@@ -1,30 +1,47 @@
-import pymongo
-from typing import Dict
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 class CarModel:
-    collection = pymongo.MongoClient().db.cars
-
-    def __init__(self, car_id: str, make: str, model: str, year: int, color: str):
-        self.car_id = car_id
+    collection = None
+    def __init__(self, make: str, model: str, year: int, color: str):
+        self._id = ObjectId()
         self.make = make
         self.model = model
         self.year = year
         self.color = color
-    def to_dict(self)-> Dict:
-        return vars(self)
+
+    def to_dict(self):
+        return {
+            "_id": str(self._id),
+            "make": self.make,
+            "model": self.model,
+            "year": self.year,
+            "color": self.color
+        }
 
     @staticmethod
-    async def find_one(car_id: str) -> Dict:
-        car = CarModel.collection.find_one({'car_id': car_id})
-        return car
-
+    def create_mongodb_client(uri: str):
+        client = MongoClient(uri)
+        CarModel.collection = client["your_database_name"]["your_collection_name"]
+        
     @staticmethod
-    async def update(car_id: str, car: Dict) -> None:
-        CarModel.collection.update_one({'car_id': car_id}, {'$set': car})
-
+    def create(car: dict):
+        return CarModel.collection.insert_one(car)
+    
     @staticmethod
-    async def delete_one(car_id: str) -> Dict:
-        return CarModel.collection.delete_one({'car_id': car_id})
-
-    async def save(self):
-        CarModel.collection.insert_one(self.to_dict())
+    def get(car_id: str):
+        return CarModel.collection.find_one({"_id": ObjectId(car_id)})
+    
+    @staticmethod
+    def get_all():
+        cars_data = list(CarModel.collection.find({}))
+        cars = [CarModel(**car_data) for car_data in cars_data]
+        return cars
+    
+    @staticmethod
+    def update(car_id: str, car: dict):
+        return CarModel.collection.update_one({"_id": ObjectId(car_id)}, {"$set": car})
+    
+    @staticmethod
+    def delete(car_id: str):
+        return CarModel.collection.delete_one({"_id": ObjectId(car_id)})
