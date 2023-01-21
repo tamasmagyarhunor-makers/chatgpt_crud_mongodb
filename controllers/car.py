2b997369dd21dfd02f068b106e3f2ac1126ea249
Magyar-Hunor Tamas <magyar-hunor.tamas@makers.tech>
2023-01-21 11:23:19 +0000

    Update Car controller

--- controllers/car.py
+++ controllers/car.py
@@ -1,23 +1,25 @@
 from typing import Dict, List
-import pymongo
+from models.car import CarModel
 
 class CarController:
-
     @staticmethod
     async def get(car_id: str) -> Dict:
-        car = await CarModel.find_one(car_id)
-        return car
+        car = CarModel.get(car_id)
+        return car.to_dict() if car else {}
+    
+    @staticmethod
+    async def get_all() -> List[Dict]:
+        cars = CarModel.get_all()
+        return [car.to_dict() for car in cars]
 
     @staticmethod
-    async def create(car: CarModel) -> Dict:
-        await car.save()
-        return car.to_dict()
-
+    async def create(car: Dict) -> Dict:
+        return CarModel.create(car)
+    
     @staticmethod
-    async def update(car_id: str, car: CarModel) -> Dict:
-        await CarModel.update(car_id, car)
-        return await CarModel.find_one(car_id)
-
+    async def update(car_id: str, car: Dict) -> Dict:
+        return CarModel.update(car_id, car)
+    
     @staticmethod
     async def delete(car_id: str) -> Dict:
-        return await CarModel.delete_one(car_id)
+        return CarModel.delete(car_id)
