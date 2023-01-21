06ce3e800e21f51dfcefe4399c37a85ebb3739e8
Magyar-Hunor Tamas <magyar-hunor.tamas@makers.tech>
2023-01-21 11:23:42 +0000

    Update Car route

--- routes/car.py
+++ routes/car.py
@@ -1,19 +1,39 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from bson.objectid import ObjectId
+from typing import Dict, List
+from controllers.car import CarController
 
 app = FastAPI()
 
-@app.get("/cars/{car_id}")
+class CarCreate(BaseModel):
+    make: str
+    model: str
+    year: int
+    color: str
+
+class CarUpdate(BaseModel):
+    make: str
+    model: str
+    year: int
+    color: str
+
+@app.post("/cars", response_model=Dict)
+async def create_car(car: CarCreate):
+    return await CarController.create(car.dict())
+
+@app.get("/cars/{car_id}", response_model=Dict)
 async def read_car(car_id: str):
     return await CarController.get(car_id)
 
-@app.post("/cars")
-async def create_car(car: CarModel):
-    return await CarController.create(car)
+@app.get("/cars", response_model=List[Dict])
+async def read_cars():
+    return await CarController.get_all()
 
-@app.put("/cars/{car_id}")
-async def update_car(car_id: str, car: CarModel):
-    return await CarController.update(car_id, car)
+@app.put("/cars/{car_id}", response_model=Dict)
+async def update_car(car_id: str, car: CarUpdate):
+    return await CarController.update(car_id, car.dict())
 
-@app.delete("/cars/{car_id}")
+@app.delete("/cars/{car_id}", response_model=Dict)
 async def delete_car(car_id: str):
     return await CarController.delete(car_id)
